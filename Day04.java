7b616c6706be637565a96c14769980b5aa0419f1
Chiang Fong Lee <myself@cflee.net>
2022-12-04 14:18:00 +0000

    Day 04 part 1

--- Day04.java
+++ Day04.java
@@ -10,6 +10,22 @@ public class Day04 {
     }
 
     static void part1(final List<String> input) {
+        int sum = 0;
+        for (String line : input) {
+            String[] pairs = line.split(",");
+            String[] pair1 = pairs[0].split("-");
+            String[] pair2 = pairs[1].split("-");
+            int p1a = Integer.parseInt(pair1[0]);
+            int p1b = Integer.parseInt(pair1[1]);
+            int p2a = Integer.parseInt(pair2[0]);
+            int p2b = Integer.parseInt(pair2[1]);
+            if (p2a >= p1a && p2b <= p1b) {
+                sum += 1;
+            } else if (p1a >= p2a && p1b <= p2b) {
+                sum += 1;
+            }
+        }
+        System.out.println(sum);
     }
 
     static void part2(final List<String> input) {

316ed1ee581ebb186e0158f7705ba10f08c34a1a
Chiang Fong Lee <myself@cflee.net>
2022-12-06 05:21:28 +0000

    Day 06 part 1

--- Day06.java
+++ Day06.java
@@ -1,15 +1,38 @@
-import java.util.List;
+import java.util.*;
 
 public class Day06 {
     public static void main(final String[] args) throws Exception {
         final List<String> input = Utils.readFile(
-                "06-sample.txt"
-                //"06.txt"
+        //        "06-sample.txt"
+                "06.txt"
                 );
-        part1(input);
+        System.out.println(7 == part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+        System.out.println(5 == part1("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+        System.out.println(6 == part1("nppdvjthqldpwncqszvftbrmjlhg"));
+        System.out.println(10 == part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+        System.out.println(11 == part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+        System.out.println(part1(input.get(0)));
     }
 
-    static void part1(final List<String> input) {
+    static boolean uniqueRange(char[] chars, int start, int end) {
+        Set<Character> seen = new HashSet<>();
+        for (int i = start; i < end; i++) {
+            if (seen.contains(chars[i])) {
+                return false;
+            }
+            seen.add(chars[i]);
+        }
+        return true;
+    }
+
+    static int part1(final String input) {
+        char[] chars = input.toCharArray();
+        for (int i = 4; i <= chars.length; i++) {
+            if (uniqueRange(chars, i - 4, i)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     static void part2(final List<String> input) {

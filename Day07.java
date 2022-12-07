2c5b271a3f15512a5d7f4fad219e4e607ce539ed
Chiang Fong Lee <myself@cflee.net>
2022-12-07 07:39:51 +0000

    Day 07 start

---
+++ Day07.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Day07 {
+    public static void main(final String[] args) throws Exception {
+        final List<String> input = Utils.readFile(
+                "07-sample.txt"
+                //"07.txt"
+                );
+        part1(input);
+    }
+
+    static void part1(final List<String> input) {
+    }
+
+    static void part2(final List<String> input) {
+    }
+}

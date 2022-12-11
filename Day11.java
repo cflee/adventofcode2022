5360a53816eecfca81a98db2d3537e0f9f78ed52
Chiang Fong Lee <myself@cflee.net>
2022-12-11 05:23:47 +0000

    Day 11 start

---
+++ Day11.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Day11 {
+    public static void main(final String[] args) throws Exception {
+        final List<String> input = Utils.readFile(
+                "11-sample.txt"
+                //"11.txt"
+                );
+        part1(input);
+    }
+
+    static void part1(final List<String> input) {
+    }
+
+    static void part2(final List<String> input) {
+    }
+}
